0a705e0a330f8b15efb815818489f78e3b20193a
Dario Turin <dturin1997@gmail.com>
2022-05-03 16:55:54 -0500

    Segundo Commit

--- series/urls.py
+++ series/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^series/$', views.JSONResponse.serie_list),
-    re_path(r'^series/(?P<pk>[0-9]+)/$', views.JSONResponse.serie_detail),
+    re_path(r'^series/$', views.serie_list),
+    re_path(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
 ]
--- series/views.py
+++ series/views.py
@@ -14,47 +14,47 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
-    @csrf_exempt
-    def serie_list(request):
-        """
-        List all code serie, or create a new serie.
-        """
-        if request.method == 'GET':
-            series = Serie.objects.all()
-            serializer = SerieSerializer(series, many=True)
+@csrf_exempt
+def serie_list(request):
+    """
+    List all code serie, or create a new serie.
+    """
+    if request.method == 'GET':
+        series = Serie.objects.all()
+        serializer = SerieSerializer(series, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SerieSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def serie_detail(request, pk):
+    """
+    Retrieve, update or delete a serie.
+    """
+    try:
+        serie = Serie.objects.get(pk=pk)
+    except Serie.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SerieSerializer(serie)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SerieSerializer(serie, data=data)
+        if serializer.is_valid():
+            serializer.save()
             return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
 
-        elif request.method == 'POST':
-            data = JSONParser().parse(request)
-            serializer = SerieSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return JSONResponse(serializer.data, status=201)
-            return JSONResponse(serializer.errors, status=400)
-
-    @csrf_exempt
-    def serie_detail(request, pk):
-        """
-        Retrieve, update or delete a serie.
-        """
-        try:
-            serie = Serie.objects.get(pk=pk)
-        except Serie.DoesNotExist:
-            return HttpResponse(status=404)
-
-        if request.method == 'GET':
-            serializer = SerieSerializer(serie)
-            return JSONResponse(serializer.data)
-
-        elif request.method == 'PUT':
-            data = JSONParser().parse(request)
-            serializer = SerieSerializer(serie, data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return JSONResponse(serializer.data)
-            return JSONResponse(serializer.errors, status=400)
-
-        elif request.method == 'DELETE':
-            serie.delete()
-            return HttpResponse(status=204)
+    elif request.method == 'DELETE':
+        serie.delete()
+        return HttpResponse(status=204)
 
